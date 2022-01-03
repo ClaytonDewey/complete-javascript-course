b01d2a3e672cb3b0de8be048965933ef020268b0
Clay <clay@dryadmedia.com>
2022-01-03 12:10:37 -0600

    Start Functions lesson

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,10 +11,12 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
-*/
 
+
+///////////////////////////////////////
+// Functions
 function logger() {
-  console.log('My name is Clayton');
+  console.log('My name is Jonas');
 }
 
 // calling / running / invoking function
@@ -23,10 +25,19 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+*/
