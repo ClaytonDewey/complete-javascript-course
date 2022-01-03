5926184ef06d0a59ddf37303e2767e20a46087d7
Clay <clay@dryadmedia.com>
2022-01-03 14:34:49 -0600

    Complete coding challenge #1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -87,9 +87,11 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
-*/
 
-const calcAge = function(birthYear) {
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYear) {
   return 2037 - birthYear;
 }
 
@@ -105,5 +107,49 @@ const yearsUntilRetirement = function (birthYear, firstName) {
     return -1;
   }
 }
+
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+const calcAverage = (val1, val2, val3) => {
+  return (val1 + val2 + val3) / 3;
+}
+
+const avgDolhins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+
+const checkWinner = (avg1, avg2) => {
+  if (avg1 >= avg2 * 2) {
+    return `Dolphins win (${avgDolhins} vs. ${avgKoalas})`; 
+  } else if (avg2 >= avg1 * 2) {
+    return `Koalas win (${avgKoalas} vs. ${avgDolhins})`;
+  } else {
+    return `There was no winner.`
+  }
+}
+
+console.log(checkWinner(avgDolhins, avgKoalas));
