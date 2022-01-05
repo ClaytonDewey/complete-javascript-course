73a0554a81ebbfd35983868b3fc12029e7555ec5
Clay <clay@dryadmedia.com>
2022-01-05 10:20:08 -0600

    Looping Arrays, Breaking & Continuing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -384,52 +384,106 @@ GOOD LUCK 😀
 
 /*
 const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBmi: function() {
-        this.bmi = (this.mass / this.height ** 2).toFixed(2);
-        return this.bmi;
-    }
-}
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
 
 const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBmi: function() {
-        this.bmi = (this.mass / this.height ** 2).toFixed(2);
-        return this.bmi;
-    }
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
 }
 
-mark.calcBmi();
-john.calcBmi();
 
-console.log(mark.bmi);
-console.log(john.bmi);
+///////////////////////////////////////
+// Iteration: The for Loop
 
-mark.bmi > john.bmi ? console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`) : console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+// console.log('Lifting weights repetition 1 🏋️‍♀️');
+// console.log('Lifting weights repetition 2 🏋️‍♀️');
+// console.log('Lifting weights repetition 3 🏋️‍♀️');
+// console.log('Lifting weights repetition 4 🏋️‍♀️');
+// console.log('Lifting weights repetition 5 🏋️‍♀️');
+// console.log('Lifting weights repetition 6 🏋️‍♀️');
+// console.log('Lifting weights repetition 7 🏋️‍♀️');
+// console.log('Lifting weights repetition 8 🏋️‍♀️');
+// console.log('Lifting weights repetition 9 🏋️‍♀️');
+// console.log('Lifting weights repetition 10 🏋️‍♀️');
 
-
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
 
 
 */
 
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
 
+// console.log(jonas[0]);
+// console.log(jonas[1]);
+// ...
+// console.log(jonas[4]);
+// jonas[5] does not exist
 
+for(let i = 0; i < jonas.length; i++) {
+    // Reading the jonas array
+    console.log(jonas[i], typeof jonas[i]);
 
-// console.log('Lifting weights repetition 1 🏆');
-// console.log('Lifting weights repetition 2 🏆');
-// console.log('Lifting weights repetition 3 🏆');
-// console.log('Lifting weights repetition 4 🏆');
-// console.log('Lifting weights repetition 5 🏆');
-// console.log('Lifting weights repetition 6 🏆');
-// console.log('Lifting weights repetition 7 🏆');
-// console.log('Lifting weights repetition 8 🏆');
-// console.log('Lifting weights repetition 9 🏆');
-// console.log('Lifting weights repetition 10 🏆');
-
-for(let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} 🏆`);
+    // Filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
 }
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for(let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+};
+console.log(ages);
+
+// continue and break
+console.log('--- ONLY STRINGS ---')
+for(let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for(let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
